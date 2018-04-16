54116fc6608e05212a2f8830e23aac86e6ff539e
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-04-15 23:08:08 -0400

    update im2txt examples and add better comments

---
+++ im2txt/receivesOnly/script.js
@@ -0,0 +1,44 @@
+// Copyright (C) 2018 Cristobal Valenzuela
+// 
+// This file is part of RunwayML.
+// 
+// RunwayML is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// RunwayML is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with RunwayML.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ===============================================================
+//
+// Runway: im2txt receiving data Demo
+// This example just receives incoming data from Runway.
+// No need to send images or data from here.
+
+// Create a connection to the Runway HTTP Server
+// *You should update this address to match the URL provided by the app
+var socket = io.connect('http://127.0.0.1:3333');
+
+// Wait until the page is loaded
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // Get the DOM log element
+  var status = document.getElementById('status');
+  var log = document.getElementById('log');
+  
+  // When a connection is established
+  socket.on('connect', function() {
+    status.innerHTML = 'Connected';
+  });
+  
+  // When there is a data event, update the log element
+  socket.on('data', function(data) {
+    log.innerHTML = data.results[0].caption;
+  });
+});
---
+++ im2txt/sendImages/script.js
@@ -0,0 +1,85 @@
+// Copyright (C) 2018 Cristobal Valenzuela
+// 
+// This file is part of RunwayML.
+// 
+// RunwayML is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// RunwayML is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with RunwayML.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ===============================================================
+//
+// Runway: im2txt Webcam Demo
+// This example sends images from your webcam and receives incoming data from Runway.
+
+// Create a connection to the Runway HTTP Server
+// *You should update this address to match the URL provided by the app
+var socket = io.connect('http://10.0.1.5:33100/query');
+
+// Wait until the page is loaded
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Get the DOM elements
+  var status = document.getElementById('status');
+  var log = document.getElementById('log');
+  var startBtn = document.getElementById('start');
+  var stopBtn = document.getElementById('stop');
+  var video = document.querySelector('video');
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+  var localMediaStream = null;
+  var shouldLoop = false;
+
+  // Get the user's camera
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+      });
+  }
+
+  // When a connection is established
+  socket.on('connect', function() {
+    status.innerHTML = 'Connected';
+  });
+
+  // When there is new data coming in, update the log element
+  socket.on('update_response', function(data) {
+    log.innerHTML = data.results[0].caption;
+    if (shouldLoop) {
+      sendImage();
+    }
+  });
+
+  // Get the current frame and send it to Runway using the Canvas API
+  function sendImage() {
+    ctx.drawImage(video, 0, 0, 300, 280);
+    // Send to Runway the current element in the canvas
+    socket.emit('update_request', {
+      data: canvas.toDataURL('image/jpeg')
+    });
+  }
+
+  // Start the loop: send an image, wait for response, send another one...
+  function start() {
+    shouldLoop = true;
+    sendImage()
+  }
+
+  // Stop the loop
+  function stop() {
+    shouldLoop = false;
+  }
+
+  // Listen to start and stop event
+  startBtn.addEventListener('click', start, false);
+  stopBtn.addEventListener('click', stop, false);
+});
